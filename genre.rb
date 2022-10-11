537c6a4f8dd608f496f83f4616ee8758be1d1ad9
ifzyy <johnsonnifemi8@gmail.com>
2022-10-11 16:45:45 +0100

    fixed linters

--- genre.rb
+++ genre.rb
@@ -1,22 +1,20 @@
 require './music'
 
-class Genre 
-    attr_accessor :name
-    attr_reader :items
-    
-    def initialize(name)
-     @id = Random.rand(1000)
-     @name = name
-     puts @name
-     @items = []
-    end
+class Genre
+  attr_accessor :name
+  attr_reader :items
 
-    def add_item(name)
-     name.genre = self
-     @items << name
-    end
+  def initialize(name)
+    @id = Random.rand(1000)
+    @name = name
+    puts @name
+    @items = []
+  end
+
+  def add_item(name)
+    name.genre = self
+    @items << name
+  end
 end
 
 genre = Genre.new('the last one')
-
-
